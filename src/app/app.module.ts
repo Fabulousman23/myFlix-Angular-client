a4626233a38e4466f497ca832adb3f689080acff
Fabulousman23 <andrew.makiev@gmail.com>
2022-12-01 12:08:51 -0500

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,14 +18,20 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const appRoutes: Routes = {
-  { path: 'welcome', component: WelcomePageComponent},
-  { path: 'movies', component: MovieCardComponent},
-  { path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
-};
+// const appRoutes: Routes = {
+//   { path: 'welcome', component: WelcomePageComponent},
+//   { path: 'movies', component: MovieCardComponent},
+//   { path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
+// };
 
 @NgModule({
-  declarations: [AppComponent, UserRegistrationFormComponent, UserLoginFormComponent, MovieCardComponent, WelcomePageComponent],
+  declarations: [
+    AppComponent,
+    UserRegistrationFormComponent,
+    UserLoginFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
