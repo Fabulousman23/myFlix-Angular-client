82d08cd9c6dfcb7548d493e01a337c22faaa91c9
Fabulousman23 <andrew.makiev@gmail.com>
2022-12-01 11:08:18 -0500

    add all movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,12 @@ export class AppComponent {
   }
   openUserLoginFormComponent(): void {
     this.dialog.open(UserLoginFormComponent, {
+      // Assigning the dialog a width
+      width: '280px',
+    });
+  }
+  openMovieDialog(): void {
+    this.dialog.open(MovieCardComponent, {
       // Assigning the dialog a width
       width: '280px',
     });
